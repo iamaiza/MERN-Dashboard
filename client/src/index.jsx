1ab19da7c9e40d191736d80aca8fa8fbe489f7f5
iamaiza <aiza9382@gmail.com>
2023-03-04 16:32:02 +0500

    initial commit

---
+++ client/src/index.jsx
@@ -0,0 +1,8 @@
+export { default as Board} from './components/dashboard/Board'
+export { default as  Customer} from './components/dashboard/Customer'
+export { default as Orders} from './components/dashboard/Orders'
+export { default as Product } from './components/dashboard/Product'
+export { default as Sales } from './components/dashboard/Sales'
+export { default as Staff } from './components/dashboard/Staff'
+export { default as Supplier } from './components/dashboard/Supplier'
+export { default as Dashboard } from './components/dashboard/Dashboard'
