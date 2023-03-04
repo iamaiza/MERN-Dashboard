c8eea1284afaa13ad3ac4863881b7feabf007fee
iamaiza <aiza9382@gmail.com>
2023-03-04 16:43:51 +0500

    dashboard commit

--- client/src/components/dashboard/List.jsx
+++ client/src/components/dashboard/List.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
 const List = () => {
-  return (
-    <div>List</div>
-  )
-}
+    return (
+        <div className="pt-16 min-h-screen">
+            <ul className="bg-gray-500 py-5 -mt-2 dashboard-list">
+                <li className="py-2 px-5 text-white">
+                    <Link to='/dashboard/board'>Dashboard</Link>
+                </li>
+                <li className="py-2 px-5 text-white">
+                    <Link to='/dashboard/customer'>Customers</Link>
+                </li>
+                <li className="py-2 px-5 text-white ">
+                    <Link to='/dashboard/orders'>Orders</Link>
+                </li>
+                <li className="py-2 px-5 text-white ">
+                    <Link to='/dashboard/products'>Products</Link>
+                </li>
+                <li className="py-2 px-5 text-white ">
+                    <Link to='/dashboard/sales'>Sales</Link>
+                </li>
+                <li className="py-2 px-5 text-white ">
+                    <Link to='/dashboard/admin'>Staff</Link>
+                </li>
+                <li className="py-2 px-5 text-white ">
+                    <Link to='/dashboard/supplier'>Suppliers</Link>
+                </li>
+            </ul>
+        </div>
+    );
+};
 
-export default List
+export default List;
