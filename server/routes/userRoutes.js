f3085e6aff4b2370415f1830f264fa0146639f84
iamaiza <aiza9382@gmail.com>
2023-03-10 20:07:28 +0500

    added user routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,13 @@
 const express = require("express");
-const { getAllCustomers, getAllAdmins, getAllSuppliers } = require("../controllers/userController.js");
+const { getAllCustomers, getAllAdmins, getAllSuppliers, createNewUser, updateUser, deleteUser, filterUsers } = require("../controllers/userController.js");
 const router = new express.Router();
-const User = require("../models/userModel")
 
+router.post("/newUser", createNewUser)
 router.get("/customer", getAllCustomers);
 router.get("/admin", getAllAdmins);
 router.get("/supplier", getAllSuppliers);
+router.put("/update/:id", updateUser)
+router.delete("/delete/:id", deleteUser)
+router.get("/users", filterUsers)
 
 module.exports = router;
